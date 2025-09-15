36f6bb91ddf7dcd751dd2d3744ce970bd0673461
Miia-iths <miia.larsson@iths.se>
2025-09-15 13:10:38 +0200

    added "rain"

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld
 {
     public static void main(String[] args)
     {
-        System.out.println("HelloWorld");
+        System.out.println();
+        System.out.println("HelloWorld! I'm singing in the rain *';,'*");
+        System.out.println();
     }
 }
