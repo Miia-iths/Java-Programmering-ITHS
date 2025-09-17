914cea8927cbbe5d430f2f3ded9da58f0dadcc13
mayacelium <mayacelium8@gmail.com>
2025-09-17 16:21:12 +0200

    17sep

---
+++ excercises.java
@@ -0,0 +1,14 @@
+public class excercises
+{
+    public static void main(String[] args)
+    {
+        //E1 1.10
+        /*
+        int a = 10, b = 20, c = 30;
+        int average = ( a + b + c ) / 3;
+        System.out.println("average: " + average);
+        */
+
+        
+    }
+}
