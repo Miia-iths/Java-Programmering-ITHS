417e3ab20129ad3d5e86721414862ee49e61d554
mayacelium <mayacelium8@gmail.com>
2025-09-22 00:37:15 +0200

    21sep25

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-public class newCode
+public class HelloWorld
 {
 public static void main(String[] args)
     {
--- excercises.java
+++ excercises.java
@@ -2,6 +2,35 @@ public class excercises
 {
     public static void main(String[] args)
     {
+        //E1 1.9
+        /*
+        i. 9 % 2 == 0;
+            9%2 =1 så inte 0
+        j. 8 >= 7 && 3 != 4;
+            8 större än eller lika med 7 och 3 är inte = 4
+        k. !(6 < 10) || (5 > 7 && 2 == 2);
+            är 6 inte mindre än 10 eller 5 större än 7 & 2 = 2
+            så nej
+        l. 'A' == 65 && 'B' != 66;
+            är A = 65 och B inte = 66
+        m. (3 + 2) * (8 / 4) == 10;
+
+        n. 4 * 3 - 2 + 8 / 4 != 4;
+            
+        o. "apple" == "apple";
+            ja det är lika
+        p. "apple" != "banana";
+            ja det är inte lika
+        q. 5 > 4 || 3 < 2 && 1 == 1;
+            true
+        r. true && false || true;
+            sant och falskt eller sant? error?
+        s. false ^ true && true;
+            true?
+        t. false || true && false;
+            what..
+        */
+        
         //E1 1.10
         /*
         int a = 10, b = 20, c = 30;
@@ -26,6 +55,23 @@ public class excercises
         */
 
         //E1 1.12
+        /*
+        String firstName = "Miia";
+        String lastName = "Larsson";
+        String fullName = firstName + " " + lastName;
+        int lengthOfFullName = fullName.length();
+        System.out.println("Full name: " + fullName + ", Length: " + lengthOfFullName);
+         */
+
+        //E1 1.13
+        /*
+        int x = 15, y = 4;
+        double quotient = (double) x / y;
+        int remainder = x % y;
+        System.out.println("Quotient: " + quotient + " Remainder: " + remainder);
+        */
+
         
+
     }
 }
