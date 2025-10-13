67f583b0b349a3d63231426a01f7bc2da248fce8
Miia-iths <miia.larsson@iths.se>
2025-10-13 14:01:40 +0200

    13oct25

--- sol/TestExceptions.java
+++ sol/TestExceptions.java
@@ -1,15 +1,15 @@
-import java.util.*;
-
 public class TestExceptions {
     public static void main(String[] args) {
-       try {
+        
+      
+      
+      
+        /*try {
             int result = 10/0;
        } catch (ArithmeticException e) {
         System.out.println("kaos" + e.getMessage());
-       }
-       
-       
-       
+       }*/
+        
     }
 }
     
